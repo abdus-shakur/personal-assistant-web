288bdf03c6f10d95cc206d86da5435c80dd5909e
Shakur <abdusshakurt@gmail.com>
2024-03-26 13:19:49 +0530

    Fixed Radio Player and Sorted Channels List

--- src/pages/Media/Radio.jsx
+++ src/pages/Media/Radio.jsx
@@ -1,44 +1,103 @@
-import { PlayArrow, RadioOutlined } from "@mui/icons-material";
-import { Button, IconButton, List, ListItem, ListItemButton, ListItemIcon, ListItemText, TextField } from "@mui/material";
+import { PlayArrow } from "@mui/icons-material";
+import {
+  IconButton,
+  List,
+  ListItemIcon,
+  ListItemText,
+  MenuItem,
+  Paper,
+  TextField,
+  Typography
+} from "@mui/material";
 import { useState } from "react";
-let stationData =  require('./stationData.json');
+let stationDataInput = require("./stationData.json");
+
 
 export default function Radio() {
- 
+  
   const [radioIndex, setRadioIndex] = useState(0);
-  const [currentStations,setStations] = useState(stationData)
-  const changeChannel = () => {
-    setRadioIndex(radioIndex==0?1:0);
+  const [stationData,setStationData] = useState(stationDataInput.sort((a,b)=>{
+    if(a.name<b.name){
+      return -1
+    }
+    if(a.name>b.name){
+      return 1
+    }
+    return 0
+  }))
+  const [currentStations, setStations] = useState(
+    [...Array(stationData.length).keys()].map((foo) => foo + 1)
+  );
+  const searchRadioStations = (event) => {
+    var matchedStations = [];
+    for (let i = 0; i < stationData.length; i++) {
+      let data = stationData[i];
+      if (
+        data.name !== null &&
+        data.name !== undefined &&
+        data.name.match(new RegExp(".*" + event.target.value + ".*", "i"))
+      ) {
+        matchedStations.push(i + 1);
+      }
+    }
+    setStations([...matchedStations]);
   };
-  const searchRadioStations = (event) =>{
-    console.log(`.*${event.target.value}.*`)
-    setStations(stationData.filter(data=>data.name !== null && data.name !== undefined).filter(data => data.name.match(new RegExp(".*"+event.target.value+".*",'i','i'))))
-  };
-  console.log(currentStations)
   return (
     <>
-    <TextField label="Search" placeholder="Search Radio Stations" onChange={(e)=>searchRadioStations(e)}></TextField>
-    <List>
-        
-    {currentStations?currentStations.map((radio,index)=>
-        <ListItem style={{width:'100%'}}>
-            <ListItemText primary={radio.name}></ListItemText>
-            <ListItemIcon style={{minWidth:'1rem'}}>
-                <IconButton onClick={()=>setRadioIndex(index)}>
+      <TextField
+        label="Search"
+        placeholder="Search Radio Stations"
+        onChange={(e) => searchRadioStations(e)}
+      ></TextField>
+      <List>
+        {currentStations ? (
+          currentStations.map((stationIndex, index) => (
+            <MenuItem
+              style={{ width: "100%" }}
+              onClick={() => setRadioIndex(stationIndex - 1)}
+            >
+              <ListItemText
+                primary={stationData[stationIndex - 1].name}
+              ></ListItemText>
+              <ListItemIcon style={{ minWidth: "1rem" }}>
+                <IconButton onClick={() => setRadioIndex(stationIndex - 1)}>
                   <PlayArrow />
                 </IconButton>
-              </ListItemIcon >
-        </ListItem>
-        
-    ):<></>}
-    </List>
-    <label>{currentStations[radioIndex]?.name}</label>
-      
-      <div style={{position:'sticky',bottom:0}}>
-        <audio src={currentStations[radioIndex]?.urls[0]} controls autoplay="false" muted>
-        </audio>
-      </div>
-      <Button onClick={changeChannel}>Change Channel</Button>
+              </ListItemIcon>
+            </MenuItem>
+          ))
+        ) : (
+          <></>
+        )}
+      </List>
+      <div style={{ height: "6.8rem" }}></div>
+      <Paper elevation={10}>
+        <Paper
+          elevation={10}
+          sx={{
+            padding: "0.5rem 0.5rem 0 0.5rem",
+            position: "fixed",
+            bottom: 0,
+            left: 0,
+            maxWidth: "100% !important",
+            width: "100%",
+            height: "6.8rem",
+          }}
+        >
+          <label style={{ width: "100%", textAlign: "center" }}>
+            <Typography variant="h6">
+              {stationData[radioIndex]?.name}
+            </Typography>
+          </label>
+          <audio
+            style={{ width: "100%" }}
+            src={stationData[radioIndex]?.urls[0]}
+            controls
+            autoplay="false"
+            muted
+          ></audio>
+        </Paper>
+      </Paper>
     </>
   );
 }
